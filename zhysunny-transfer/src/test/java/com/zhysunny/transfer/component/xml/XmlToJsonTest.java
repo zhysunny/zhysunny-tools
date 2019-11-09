9f3a40e57b6247615b642d8848275c645bfb3728
zhangyun <0049003310@znv.com>
2019-11-09 15:38:01 +0800

    xml读写重构

--- zhysunny-transfer/src/test/java/com/zhysunny/transfer/component/xml/XmlToJsonTest.java
+++ zhysunny-transfer/src/test/java/com/zhysunny/transfer/component/xml/XmlToJsonTest.java
@@ -3,6 +3,7 @@ package com.zhysunny.transfer.component.xml;
 import static org.junit.Assert.*;
 import com.alibaba.fastjson.JSONObject;
 import com.zhysunny.io.xml.XmlReader;
+import com.zhysunny.transfer.constant.Constants;
 import com.zhysunny.transfer.mapping.Mapping;
 import org.junit.*;
 import java.util.List;
@@ -40,7 +41,7 @@ public class XmlToJsonTest {
      */
     @Test
     public void testRead1() throws Exception {
-        mapping = new XmlReader(classLoader.getResource("example/xml/1/xml_to_xml.xml")).read(Mapping.class);
+        mapping = new XmlReader("example/xml/1/xml_to_xml.xml").read(Mapping.class);
         List<JSONObject> datas = (List<JSONObject>)new XmlReader(classLoader.getResource("example/xml/1/input.xml"))
         .read(new XmlToJson(mapping));
         System.out.println(datas);
@@ -55,9 +56,9 @@ public class XmlToJsonTest {
      */
     @Test
     public void testRead2() throws Exception {
-        mapping = new XmlReader(classLoader.getResource("example/xml/2/xml_to_xml.xml")).read(Mapping.class);
-        List<JSONObject> datas = (List<JSONObject>)new XmlReader(classLoader.getResource("example/xml/2/input.xml"))
-        .read(new XmlToJson(mapping));
+        Constants.XML_DATA_NODE_FROM = "property";
+        mapping = new XmlReader("example/xml/2/xml_to_xml.xml").read(Mapping.class);
+        List<JSONObject> datas = (List<JSONObject>)new XmlReader("example/xml/2/input.xml").read(new XmlToJson(mapping));
         System.out.println(datas);
         assertEquals(datas.size(), 3);
         JSONObject json = datas.get(0);
@@ -71,6 +72,7 @@ public class XmlToJsonTest {
      */
     @Test
     public void testRead3() throws Exception {
+        Constants.XML_DATA_NODE_FROM = "Topic";
         mapping = new XmlReader(classLoader.getResource("example/xml/3/xml_to_xml.xml")).read(Mapping.class);
         List<JSONObject> datas = (List<JSONObject>)new XmlReader(classLoader.getResource("example/xml/3/input.xml"))
         .read(new XmlToJson(mapping));
