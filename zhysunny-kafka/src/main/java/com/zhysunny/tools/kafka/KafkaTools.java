ec24e3479976b9b6ed309c0f786788e38de59f8f
zhangyun <0049003310@znv.com>
2019-12-07 15:35:37 +0800

    增加kafka工具模块

---
+++ zhysunny-kafka/src/main/java/com/zhysunny/tools/kafka/KafkaTools.java
@@ -0,0 +1,14 @@
+package com.zhysunny.tools.kafka;
+
+/**
+ * kafka工具主函数
+ * @author 章云
+ * @date 2019/12/7 14:44
+ */
+public class KafkaTools {
+
+    public static void main(String[] args) {
+
+    }
+
+}
